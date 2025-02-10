f83401ea9bd7cc00b4c44c124f8cdc315b6bd560
OsDaSP <Pekkasdani@gmail.com>
2025-02-10 11:48:00 -0600

    hola

---
+++ 10-feb/primer_programa.c
@@ -0,0 +1,8 @@
+/*Creado el 10 de febrero de 2025 po9r Oscar Daniel Sanchez*/
+#include<stdio.h>
+int main()
+{
+printf("hola oscar \n");
+printf("Este es el primer programa del curso\n");
+}
+/*Termine de escribir el programa*/
